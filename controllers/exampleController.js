5c918d562657b9d4e460f30b2fe33c49fa1be022
Julius Suominen <julius.suominen@gmail.com>
2017-07-13 19:43:06 +0200

    pug layout refactoring

--- controllers/exampleController.js
+++ controllers/exampleController.js
@@ -3,14 +3,15 @@ const User = require('../models/User');
 // With exports.something we don't need to use module.exports in the end of the file
 exports.getExamplePage = async (req, res, next) => {
 
+  // Await that User.find is ready, then render
   const users = await User.find();
   res.render('index', {
-    'title': 'This page is rendered in exampleController',
+    'title': 'This main page is rendered in exampleController',
     'ptag': 'In this sentence we send info to p tag from the controller and pug uses #{ptag} to show our data',
     users
   });
 };
 
 exports.getSignUpForm = (req, res, next) => {
-  res.render('signup');
+  res.render('signup', {'title': 'This signup page is also rendered in exampleController. Could be rendered from any controller.'});
 };
