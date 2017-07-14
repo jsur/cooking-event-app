9aba94410683481e635bc7f41c0d6cd544ea93aa
Julius Suominen <julius.suominen@gmail.com>
2017-07-14 11:37:33 +0200

    changes

--- models/User.js
+++ models/User.js
@@ -2,11 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  'username': String,
+  'email': {
+    'type': String,
+    'required': 'Email is required!'
+  },
   'password': {
     'type': Number,
     'required': 'Password is required!'
   },
+  'username': String,
+  'firstname': String,
+  'lastname': String,
   'events_attending': [{'type': Schema.Types.ObjectId, 'ref': 'Event'}],
   'events_hosting': [{'type': Schema.Types.ObjectId, 'ref': 'Event'}],
   'preferences': [String]
