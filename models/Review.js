522b1b7f9ee0d63476442e78dece393d6aa2a09d
Julius Suominen <julius.suominen@gmail.com>
2017-07-14 11:58:39 +0200

    first

---
+++ models/Review.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+  'author': [{'type': Schema.Types.ObjectId, 'ref': 'User'}],
+  'host': [{'type': Schema.Types.ObjectId, 'ref': 'User'}],
+  'rating': Number,
+  'description': String
+}, { 'timestamps': {'createdAt': 'created_at'} });
+
+const Review = mongoose.model('Review', reviewSchema);
+module.exports = Review;
