14e297cf04d70ffff97ea5b312fa3762f41dacad
Julius Suominen <julius.suominen@gmail.com>
2017-07-14 12:04:05 +0200

    email change

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,18 +4,18 @@ const User = require('../models/User');
 
 // No need to add error handling, route is wrapped with catchErrors() in index.js
 exports.makeNewUser = async (req, res, next) => {
-  const username = req.body.username;
+  const email = req.body.email;
   const password = req.body.password;
 
   const salt = await bcrypt.genSalt(bcryptSalt);
   const hashPass = await bcrypt.hash(password, salt);
 
   const newUser = User({
-    username,
+    email,
     password
   });
 
   const user = await newUser.save();
-  req.flash('success', `User ${user.username} created!`);
+  req.flash('success', `User ${user.email} created!`);
   res.redirect('/');
 };
