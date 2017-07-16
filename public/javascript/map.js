03bc1bb5e19577769ab479de6ca12a59cdc8d46b
Julius Suominen <julius.suominen@gmail.com>
2017-07-16 14:37:57 +0200

    map page get call start

---
+++ public/javascript/map.js
@@ -0,0 +1,9 @@
+// Here we can utilize our API routes with axios
+
+/* axios.get(`/api/events/near?lat=${lat}&lng=${lng}`)
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
+    console.log(error);
+  });*/
