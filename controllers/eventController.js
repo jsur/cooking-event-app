c3df1f726587dc81f8fbc70ab0ac3aa511b26b41
Julius Suominen <julius.suominen@gmail.com>
2017-07-19 19:02:18 +0200

    newevent layout

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -2,7 +2,7 @@ const Event = require('../models/Event');
 const User = require('../models/User');
 const moment = require('moment');
 const multer = require('multer');
-const upload = multer({ dest: '../public/uploads' });
+const upload = multer({ 'dest': '../public/uploads' });
 
 // With exports.something we don't need to use module.exports in the
 // end of the file
