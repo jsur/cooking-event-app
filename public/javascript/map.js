7780e55bb5b57b03be3163f6c84ba3f3ea9c4021
Camille DW <camille.dewulf@gmail.com>
2017-07-20 14:49:01 +0200

    styling event page

--- public/javascript/map.js
+++ public/javascript/map.js
@@ -119,14 +119,13 @@ function renderEvents (events) {
     $(eventsContainer).append(`
       <div class="eventcard">
         <div class="event-card-image">
-          <img src="event.image.path">
+          <img src=${event.image.path}>
         </div>
         <div class="event-card-info">
           <p class="event-title">${event.title}</p>
-          <p class="event-details">Food type: ${event.foodtype}</p>
-          <p class="event-details">Price: ${event.price}</p>
-          <p class="event-details">Date: ${event.date.slice(0, 10)}</p>
-          <p class="event-capacity-check">Places left: ${event.capacity - event.attendees.length}</p>
+          <p class="event-details">${event.foodtype} food - ${event.price}€</p>
+          <p class="event-details">${event.date.slice(0, 10)} at ${event.date.slice(11, 16)}h</p>
+          <p class="event-capacity-check">${event.capacity - event.attendees.length} places left</p>
           <a class="event-details-link" href='/event/${event._id}'>Event details</a>
         </div>
       </div>
