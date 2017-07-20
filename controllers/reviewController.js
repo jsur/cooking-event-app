d183c596a5b63da8e95ac71053f8837ee8219b9d
Julius Suominen <julius.suominen@gmail.com>
2017-07-20 11:52:44 +0200

    reviews functionality

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,11 +1,13 @@
 const Review = require('../models/Review');
+const User = require('../models/User');
 
 exports.leaveReview = async (req, res, next) => {
 
-  // res.send({'body': req.body, 'user': req.user, 'host': req.params});
+  const author = await User.find({ '_id': req.user._id });
 
   const reviewInfo = {
     'author': req.user._id,
+    'authorname': author[0].firstname + ' ' + author[0].lastname,
     'host': req.params.id,
     'rating': req.body.rating,
     'description': req.body.description
